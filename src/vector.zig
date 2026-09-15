209a660ff5be671abf1f4de6b0e0f8b08af3910f
Jack Parsons <jackpar1113@gmail.com>
2026-09-15 13:16:21 -0600

    simplifications

--- src/vector.zig
+++ src/vector.zig
@@ -2,11 +2,6 @@ const std = @import("std");
 
 pub const Vec3 = @Vector(3, f64);
 
-pub const Fmt = std.fmt.Alt(Vec3, format);
-fn format(v: Vec3, w: *std.Io.Writer) !void {
-    try w.print("{d} {d} {d}", .{ v[0], v[1], v[2] });
-}
-
 pub const Point = Vec3;
 pub const Color = Vec3;
 
@@ -24,10 +19,10 @@ pub fn z(v: Vec3) f64 {
 }
 
 pub fn magnitude(v: Vec3) f64 {
-    return @sqrt(full_magnitude(v));
+    return @sqrt(magnitude_squared(v));
 }
 
-pub fn full_magnitude(v: Vec3) f64 {
+pub fn magnitude_squared(v: Vec3) f64 {
     return @reduce(.Add, v * v);
 }
 
@@ -46,7 +41,7 @@ pub fn reflect(v: Vec3, normal: Vec3) Vec3 {
 pub fn refract(v: Vec3, normal: Vec3, refraction_ratio: f64) Vec3 {
     const cos_theta = @min(dot(-v, normal), 1.0);
     const r_out_perp = splat(refraction_ratio) * (v + splat(cos_theta) * normal);
-    const r_out_parallel = splat(-@sqrt(@abs(1 - full_magnitude(r_out_perp)))) *
+    const r_out_parallel = splat(-@sqrt(@abs(1 - magnitude_squared(r_out_perp)))) *
         normal;
 
     return r_out_perp + r_out_parallel;
@@ -98,7 +93,7 @@ pub fn randomRange(r: std.Random, min: f64, max: f64) Vec3 {
 pub fn randomUnit(r: std.Random) Vec3 {
     while (true) {
         const v = randomRange(r, -1, 1);
-        const m2 = full_magnitude(v);
+        const m2 = magnitude_squared(v);
         if (std.math.floatEpsAt(f64, 0) < m2 and m2 <= 1) {
             return v / @sqrt(splat(m2));
         }
@@ -117,7 +112,7 @@ pub fn randomHemisphere(r: std.Random, normal: Vec3) Vec3 {
 pub fn randomUnitDisk(r: std.Random) Vec3 {
     while (true) {
         const p: Vec3 = .{ r.float(f64) * 2 - 1, r.float(f64) * 2 - 1, 0 };
-        if (full_magnitude(p) < 1) {
+        if (magnitude_squared(p) < 1) {
             return p;
         }
     }
