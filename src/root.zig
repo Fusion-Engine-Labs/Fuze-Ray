6109d650371c50c90faa997ac33b8e606e012e74
Jack Parsons <jackpar1113@gmail.com>
2026-09-15 18:43:46 -0600

    Episode 1

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,7 @@
 pub const Rgba = @import("utils.zig").Rgba;
 pub const Buffer = @import("buffer.zig");
+const v = @import("vector.zig");
+const Ray = @import("ray.zig");
 
 pub const Entity = struct {
     fill: Rgba,
@@ -7,13 +9,55 @@ pub const Entity = struct {
     radius: f64,
 };
 
+fn hit_sphere(center: v.Vec3, radius: f64, ray: *const Ray) bool {
+    const oc = center - ray.origin;
+    const a = v.dot(ray.direction, ray.direction);
+    const b = -2 * v.dot(ray.direction, oc);
+    const c = v.dot(oc, oc) - (radius * radius);
+
+    const discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+        return false;
+    }
+
+    return true;
+}
+
 pub fn render(buf: *Buffer, entity: Entity) void {
+    const width: f64 = @floatFromInt(buf.width);
+    const height: f64 = @floatFromInt(buf.height);
+
+    const focal_length: f64 = 1.0;
+    const viewport_height: f64 = 2.0;
+    const viewport_width = viewport_height * width / height;
+    const camera_center = v.zero;
+
+    const viewport_u: v.Vec3 = .{ viewport_width, 0, 0 };
+    const viewport_v: v.Vec3 = .{ 0, -viewport_height, 0 };
+
+    const pixel_delta_u = viewport_u / v.splat(width);
+    const pixel_delta_v = viewport_v / v.splat(height);
+
+    const viewport_upper_left = camera_center - v.init(0, 0, focal_length) - viewport_u / v.splat(2) - viewport_v / v.splat(2);
+    const pixel00_loc = viewport_upper_left + v.splat(0.5) * (pixel_delta_u + pixel_delta_v);
+
     for (0..buf.height) |yi| {
         const y: u32 = @intCast(yi);
         for (buf.row(y), 0..) |*pixel, xi| {
-            const x: u32 = @intCast(xi);
-            _ = x;
-            pixel.* = entity.fill;
+            const pixel_center = pixel00_loc + (v.splat(xi) * pixel_delta_u) + (v.splat(yi) * pixel_delta_v);
+            const ray_direction = pixel_center - camera_center;
+
+            const ray = Ray.init(camera_center, ray_direction);
+            const c = v.init(entity.position[0], entity.position[1], entity.position[2]);
+            if (hit_sphere(c, entity.radius, &ray)) {
+                pixel.* = entity.fill;
+                continue;
+            }
+
+            const unit_direction = v.unit(ray.direction);
+            const a: f64 = 0.5 * (v.y(unit_direction) + 1.0);
+            const color = v.splat(1.0 - a) * v.one + v.splat(a) * entity.fill.toColor();
+            pixel.* = .fromColor(color);
         }
     }
 }
